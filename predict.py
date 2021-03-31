be188a75db5d266797b7609ba025a3665af07299
Saobangmath <anhtai2672000@gmail.com>
2021-03-31 13:07:34 +0800

    refactor code in predict.py

--- predict.py
+++ predict.py
@@ -1,38 +1,19 @@
-import sys
-import argparse
-
 import yaml
 
 from utils.trainer import Trainer
 
-
 DESCRIPTION = """Make prediction."""
 
+config_path = "./work_dirs/20210327_165510/config.yaml"
+load_from = "./work_dirs/20210327_165510/checkpoint_best.pth"
 
-def main(args):
-    # Initializer trainer; initialize only one cos very costly
-    trainer = load_trainer(args.config_path, args.load_from)
-
-    # Start training
-    while True:
-        text = input("Enter a review:")
-        predict_aspects(trainer, text)
-
-
-# call this function one to load the trainer;
-"""
-    config_path: the link to config.yaml file
-    load_from: load from the pretrained model 
-"""
-def load_trainer(config_path, load_from):
-    with open(config_path, "r") as conf:
-        config = yaml.load(conf, Loader=yaml.FullLoader)
-    config["action"] = "predict"
-    config["config_path"] = config_path
-    config["load_from"] = load_from
-    config["resume_from"] = None
-    trainer = Trainer(config)
-    return trainer
+with open(config_path, "r") as conf:
+    config = yaml.load(conf, Loader=yaml.FullLoader)
+config["action"] = "predict"
+config["config_path"] = config_path
+config["load_from"] = load_from
+config["resume_from"] = None
+trainer = Trainer(config)
 
 """
     return the aspects prediction for a text in the form: 
@@ -45,22 +26,17 @@ def load_trainer(config_path, load_from):
             "price_existence_preds" : [0, 1],
         }
 """
-def predict_aspects(trainer, text):
+
+
+def predict_aspects(text):
     return trainer.predict(text)
 
 
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description=DESCRIPTION)
-    parser.add_argument(
-        '-c', '--config-path', type=str, required=True,
-        help='Path to full config that the model is trained with.')
-    parser.add_argument(
-        '-m', '--load-from', type=str, required=True, help='Path to trained model.')
-
-    return parser.parse_args(argv)
+def main():
+    while True:
+        text = input("Enter a review:")
+        predict_aspects(text)
 
 
 if __name__ == '__main__':
-    main(parse_arguments(sys.argv[1:]))
+    main()
